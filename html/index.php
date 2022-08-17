3926afa2c981e010eda1eea8f1601f99792a4561
Alberto Roura <mail@albertoroura.com>
2022-08-18 09:53:20 +1000

    Update index.php

--- html/index.php
+++ html/index.php
@@ -2,7 +2,7 @@
 
 if (!isset($_GET['code'])) {
     header("HTTP/1.1 400 Bad Request");
-    exit('Code not provided');
+    exit('HTTP Code not provided.');
 }
 
 $code_list = json_decode(file_get_contents('codes.json'), true);
@@ -10,12 +10,12 @@ $codes = array_column($code_list, 'code');
 
 $requested_code = $_GET['code'];
 
-if (!is_numeric($requested_code) || !in_array($requested_code, $codes)) {
-    exit('Requested code is not valid');
+if (!in_array($requested_code, $codes)) {
+    header("HTTP/1.1 404 Not Found");
+    exit('Requested HTTP code was not found.');
 }
 
-$key = array_search($requested_code, $codes);
-$code_meta = $code_list[$key];
+$code_meta = $code_list[array_search($requested_code, $codes)];
 
 $response = "HTTP/1.1 ${requested_code} ${code_meta['phrase']}";
 
