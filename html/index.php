ba92f4e1f2d0fcd2d205efd3adbbf1f0b9cb2dff
Alberto Roura <mail@albertoroura.com>
2022-08-18 08:55:52 +1000

    Working

--- html/index.php
+++ html/index.php
@@ -1,24 +1,27 @@
 <?php
 
-$code_list = json_decode(file_get_contents('codes.json'));
+if (!isset($_GET['code'])) {
+    header("HTTP/1.1 400 Bad Request");
+    exit('Code not provided');
+}
 
-$codes = [
-    200 => 'OK',
-    301 => 'Moved Permanently',
-    400 => 'Bad Request',
-    404 => 'Not Found',
-    418 => 'I\'m a teapot',
-    500 => 'Internal Server Error'
-];
+$code_list = json_decode(file_get_contents('codes.json'), true);
+$codes = array_column($code_list, 'code');
 
 $requested_code = $_GET['code'];
 
-if (!is_numeric($requested_code) || !array_key_exists($requested_code, $codes)) {
+if (!is_numeric($requested_code) || !in_array($requested_code, $codes)) {
     exit('Requested code is not valid');
 }
 
-$response = "HTTP/1.1 ${requested_code} ${codes[$requested_code]}";
+$key = array_search($requested_code, $codes);
+$code_meta = $code_list[$key];
+
+$response = "HTTP/1.1 ${requested_code} ${code_meta['phrase']}";
 
 header($response);
 
-exit($response);
+echo "<h1>${response}</h1>";
+echo "<dl><dt><a target='_blank' href='${code_meta['spec_href']}'>${code_meta['spec_title']}</a></dt><dd>${code_meta['description']}</dd></dl>";
+echo "<hr/>";
+echo "<address>${_SERVER['SERVER_SOFTWARE']}</address>";
